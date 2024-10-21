c5c7385c81eddbb0f67a5e958c8b4f03364e7382
suramyavns <sdidwania645@gmail.com>
2024-10-21 11:16:52 +0530

    pages

--- backend/server.py
+++ backend/server.py
@@ -3,39 +3,43 @@ from crud import *
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def root():
-    return jsonify({'message':'This is URL shortener'})
+    return render_template('no-content.html')
 
-@app.route('/get_url',methods=['POST'])
+@app.route('/get_url')
 def fetch_url():
-    try:
-        uuid = request.get_json()['uuid']
-    except:
-        return jsonify({'message':'Provide correct uuid!'})
-    else:
-        url = get_url(uuid)
-        if url:
-            return jsonify({'url':url})
-        return jsonify({'message':'Invalid uuid!'})
-
-@app.route('/add_url',methods=['POST'])
+    method = request.method
+    if method=='POST':
+        try:
+            uuid = request.get_json()['uuid']
+        except:
+            return jsonify({'message':'Provide correct uuid!'})
+        else:
+            url = get_url(uuid)
+            if url:
+                return jsonify({'url':url})
+            return jsonify({'message':'Invalid uuid!'})
+    return render_template('bad-request.html')
+@app.route('/add_url')
 def insert_url():
-    data = request.get_json()
-    try:
-        uuid = add_url(str(data['url']))
-        if uuid:
-            return jsonify({'uuid':uuid})
-        return jsonify({"messsage":'Invalid url!'})
-    except:
-        return jsonify({'message':'URL not specified'})
-
+    method = request.method
+    if method=='POST':
+        data = request.get_json()
+        try:
+            uuid = add_url(str(data['url']))
+            if uuid:
+                return jsonify({'uuid':uuid})
+            return jsonify({"messsage":'Invalid url!'})
+        except:
+            return jsonify({'message':'URL not specified'})
+    return render_template('bad-request.html')
 @app.route('/<string:uuid>')
 def go_to(uuid):
     url = get_url(uuid)
     if url:
         return redirect(url)
-    return render_template('bad-request.html')
+    return render_template('not-found.html')
 
 if __name__=='__main__':
     app.run(debug=True)
